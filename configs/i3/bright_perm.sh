8ee9f633b01850c95bfe494a6e36f0dc0767a269
sStellar <rexstellar.pa@gmail.com>
2020-01-14 17:57:59 +0100

    Removed *

--- configs/i3/bright_perm.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-i3-input | grep output | cut -d'=' -f 2 | cut -d' ' -f 2 | sudo -S chmod a+w /sys/class/backlight/acpi_video0/brightness
--- configs/i3/p.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-i3-input | grep output | cut -d'=' -f 2 | cut -d' ' -f 2 | sudo chmod a+x ~/.config/i3/test
